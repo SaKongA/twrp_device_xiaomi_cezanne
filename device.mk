a067c1a82dc5f4b2e4d27be5e4e5fceeecb241de
ZhcnPanda <2623493447@qq.com>
2022-07-24 09:04:57 +0800

    Revert "cezanne: Clean device tree,import some flags"
    This reverts commit f3d4663733cbec337f81076acc33a9ffe6f16bbd.

--- device.mk
+++ device.mk
@@ -1,25 +1,23 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 The TWRP Open Source Project
+# Copyright (C) 2021 The OmniRom Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_SHIPPING_API_LEVEL := 29
+LOCAL_PATH := device/xiaomi/cezanne
 
-# Partitions
+# Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Fastbootd
+# fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
